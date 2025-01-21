696705c0675b85c9011a1650aeef56acc93881de
Matheus Vilar <70180386+MatheusVi1ar@users.noreply.github.com>
2025-01-20 22:23:25 -0300

    Add files via upload

--- lambda_function.py
+++ lambda_function.py
@@ -1,15 +1,15 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import util.scrap as scrap
 import util.constantes as constantes
 import util.aws as aws
 
 def main():
     try:
-        datenow = datetime.today().strftime('%Y%m%d')
+        datenow = (datetime.now().astimezone() - timedelta(hours=3, minutes=0)).strftime('%Y%m%d')
 
         # Extract HTML
         try:
-            parquet_buffer = scrap.scrap_html(constantes.url, datenow)
+            parquet_buffer, datenow = scrap.scrap_html(constantes.url, datenow)
         except Exception as e:
             msg = f"Error extracting HTML: {e}"
             print(msg)
@@ -19,9 +19,9 @@ def main():
         try:
             aws.enviar_parquet_s3(  parquet_buffer, 
                                     datenow, 
-                                    constantes.access_key, 
-                                    constantes.secret_key, 
-                                    constantes.session_token)
+                                    constantes.aws_access_key_id, 
+                                    constantes.aws_secret_access_key, 
+                                    constantes.aws_session_token)
         except Exception as e:
             msg = f"Error uploading to S3: {e}"
             print(msg)
@@ -33,8 +33,8 @@ def main():
         return(msg)
     return("Success")
 
-#if __name__ == '__main__':
-#    main()
+if __name__ == '__main__':
+    main()
 
 def handler(event, context):
     return main()
